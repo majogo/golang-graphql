677ec0626fd04f521ba90c8fb5bf55bf05cc7647
Maerker Jochen <jochen.maerker@gmx.net>
2018-11-10 22:04:39 +0100

    implementing artist resolvers

--- resolver.go
+++ resolver.go
@@ -18,17 +18,21 @@ func (r *Resolver) Query() QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) CreateArtist(ctx context.Context, name string, genre *Genre) (Artist, error) {
-	panic("not implemented")
+	addedArtist, err := artistRepositoryInstance.Add(name, genre)
+	if err != nil {
+		return Artist{}, err
+	}
+	return addedArtist, nil
 }
 func (r *mutationResolver) DeleteArtist(ctx context.Context, name string) (string, error) {
-	panic("not implemented")
+	return name, artistRepositoryInstance.Delete(name)
 }
 
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Artists(ctx context.Context) ([]Artist, error) {
-	panic("not implemented")
+	return artistRepositoryInstance.FindAll()
 }
 func (r *queryResolver) Artist(ctx context.Context, name string) (Artist, error) {
-	panic("not implemented")
+	return artistRepositoryInstance.FindByName(name)
 }
