d83015c4913324048773c2a7ffe538850bb00a3b
Maerker Jochen <jochen.maerker@gmx.net>
2018-11-11 16:53:46 +0100

    creating disc repository

---
+++ disc_repository.go
@@ -0,0 +1,53 @@
+package golang_graphql
+
+import "fmt"
+
+type discRepository struct {
+	discs []Disc
+}
+
+var discRepositoryInstance discRepository
+
+func init() {
+	discRepositoryInstance = discRepository{
+		discs: make([]Disc, 0),
+	}
+}
+
+func (repository *discRepository) Add(artistName, name string, year int) (Disc, error) {
+	newDisc := Disc{ArtistName: artistName, Name: name, Year: year}
+	repository.discs = append(repository.discs, newDisc)
+	return newDisc, nil
+}
+
+func (repository *discRepository) Delete(name string) error {
+	for i, disc := range repository.discs {
+		if name == disc.Name {
+			repository.discs = append(repository.discs[:i], repository.discs[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("disc with name name='%s' not found", name)
+}
+
+func (repository *discRepository) FindAll() ([]Disc, error) {
+	return repository.discs, nil
+}
+
+func (repository *discRepository) FindByName(name string) (Disc, error) {
+	for _, disc := range repository.discs {
+		if disc.Name == name {
+			return disc, nil
+		}
+	}
+	return Disc{}, fmt.Errorf("disc with name='%v' not found", name)
+}
+func (repository *discRepository) FindByArtistName(artistName string) ([]Disc, error) {
+	discs := make([]Disc, 0)
+	for _, disc := range repository.discs {
+		if disc.ArtistName == artistName {
+			discs = append(discs, disc)
+		}
+	}
+	return discs, nil
+}
