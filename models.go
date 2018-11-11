f65e871faa931ebc66a1673fa37b7c613b565bcb
Maerker Jochen <jochen.maerker@gmx.net>
2018-11-11 17:55:52 +0100

    adding disc model

--- models.go
+++ models.go
@@ -4,3 +4,9 @@ type Artist struct {
 	Name  string `json:"name"`
 	Genre *Genre `json:"genre"`
 }
+
+type Disc struct {
+	ArtistName       string  `json:"artistName"`
+	Name             string  `json:"name"`
+	Year             int     `json:"year"`
+}
